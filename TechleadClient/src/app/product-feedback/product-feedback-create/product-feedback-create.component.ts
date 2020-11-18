12985ab5a4ecf4bc12ab1297bb0bb348652158d9
George Georgallidis <George Georgallidis@DESKTOP-LD1AIV6>
2020-11-18 14:50:48 -0500

    Added the product type drop down as well as requiring the user to choose a type before seeing the product dropdown, rating selector, and the feedback text area. Also started implementing the subscription for the product type drop down.

--- TechleadClient/src/app/product-feedback/product-feedback-create/product-feedback-create.component.ts
+++ TechleadClient/src/app/product-feedback/product-feedback-create/product-feedback-create.component.ts
@@ -1,33 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {FormControl, FormGroup, FormBuilder, Validators, Form, FormsModule} from '@angular/forms';
 import {ProductFeedbackService} from '../../services/product-feedback.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-product-feedback-create',
   templateUrl: './product-feedback-create.component.html',
   styleUrls: ['./product-feedback-create.component.scss']
 })
-export class ProductFeedbackCreateComponent implements OnInit {
+export class ProductFeedbackCreateComponent implements OnInit, OnDestroy {
   createFeedbackForm: FormGroup;
   rating: FormControl;
   feedbackField: FormControl;
   productSelection: FormControl;
+  productTypeSelection: FormControl;
+  selectedProductType: boolean;
+  subscriptions: Subscription;
 
   constructor(private builder: FormBuilder, public feedbackService: ProductFeedbackService) {
     this.rating = new FormControl('', Validators.compose([Validators.required]));
     this.feedbackField = new FormControl('', Validators.compose([Validators.required]));
     this.productSelection = new FormControl('', Validators.compose([Validators.required]));
+    this.productTypeSelection = new FormControl('', Validators.compose([Validators.required]));
   }// constructor
 
   ngOnInit(): void {
     this.createFeedbackForm = this.builder.group({
       rating: this.rating,
       productSelection: this.productSelection,
-      feedbackField: this.feedbackField
+      feedbackField: this.feedbackField,
+      productTypeSelection: this.productTypeSelection
     });
+    this.selectedProductType = false;
+    this.subscriptions = new Subscription();
   }// ngOnInit
 
-  createFeedback() {
+  // Creates the feedback
+  createFeedback(): void {
     //TODO: Inplement the create feedback method
   }// createFeedback
+
+  // When the product type is changed
+  onChangeProductType(): void {
+    const xSubscription = this.createFeedbackForm.get('productTypeSelection').valueChanges.subscribe(value => {
+      console.log(value);
+      this.selectedProductType = true;
+    });
+
+    this.subscriptions.add(xSubscription);
+  }// onChangeProductType
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }// ngOnDestroy
 }// ProductFeedbackCreateComponent
