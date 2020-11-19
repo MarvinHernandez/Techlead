e47571e2d7fb3794866a025ddb9eecbcf8b6ac67
George Georgallidis <George Georgallidis@DESKTOP-LD1AIV6>
2020-11-19 15:39:27 -0500

    Finished the laptop model and started the laptop service

--- TechleadClient/src/app/services/product-laptop.service.ts
+++ TechleadClient/src/app/services/product-laptop.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import {GenericHttpService} from './generic-http.service';
-
+import { HttpClient } from '@angular/common/http';
+import { BASEURL } from '../constants';
+import { GenericHttpService} from '../services/generic-http.service';
+import {Laptop} from '../models/laptop';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductLaptopService extends GenericHttpService<Laptop>{
-
-  constructor() { }
-}
+export class LaptopService extends GenericHttpService<Laptop> {
+  constructor(public http: HttpClient) {
+    super(http, `${BASEURL}/api/laptop`);
+  } // constructor
+} // EmployeeService
