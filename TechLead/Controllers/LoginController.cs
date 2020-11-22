20c5c20d607e84f45e7d5d00c05e45fd7d498dbc
Holovatenko <juliagolovatenko182@gmail.com>
2020-11-22 15:46:20 -0500

    LoginController and LoginService update

---
+++ TechLead/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Cors;
+using TechLead.models;
+using TechLead.services;
+
+namespace TechLead.Controllers
+{
+    [EnableCors("AnotherPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+        private readonly LoginService _loginService;
+
+        public LoginController(IConfiguration config, LoginService loginService)
+        {
+            _config = config;
+            _loginService = loginService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] Member login)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = _loginService.AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = _loginService.GenerateJSONWebToken(user, _config);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }       
+    }
+}
