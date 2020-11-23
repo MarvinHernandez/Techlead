35531343ba5ef6fa8263c355cb7c80c04fc9970d
Holovatenko <juliagolovatenko182@gmail.com>
2020-11-23 18:41:07 -0500

    wishlist update (second try)

---
+++ TechleadClient/src/app/models/wishlist.ts
@@ -0,0 +1,9 @@
+/**
+ * wishlist - interface for wishlists
+ */
+export interface Wishlist {
+  id: string;
+  wishlistName: string;
+  memberId: string;
+  products: WishlistProduct[];
+}
---
+++ TechleadClient/src/app/models/wishlistProduct.ts
@@ -0,0 +1,7 @@
+/**
+ * wishlistProduct - interface for wishlist products
+ */
+export interface Wishlist {
+  productId: string;
+  productType: string;
+}
