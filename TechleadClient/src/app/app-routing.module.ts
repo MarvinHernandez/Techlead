84819e5f3b374e7a41e0cf5fa0952157bec9d7bf
anas <awmh.1997@gmail.com>
2020-11-23 23:36:51 -0500

    home component updated

--- TechleadClient/src/app/app-routing.module.ts
+++ TechleadClient/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import {MemberCreateComponent} from './member/member-create/member-create.component';
 import {MemberHomeComponent} from './member/member-home/member-home.component';
 import {ProductFeedbackCreateComponent} from './product-feedback/product-feedback-create/product-feedback-create.component';
 
 const routes: Routes = [
+  {path:'home',component:HomeComponent},
   {path: 'memberhome', component: MemberHomeComponent},
   {path: 'createaccount', component: MemberCreateComponent},
+  {path: '', component: HomeComponent},
   {path: 'createproductfeedback', component: ProductFeedbackCreateComponent}
 ];
 
